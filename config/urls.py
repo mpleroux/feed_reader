3ae40996a59a765f40f3982b3e3f758833737029
Michael LeRoux <946718+mpleroux@users.noreply.github.com>
2026-06-18 23:20:24 -0400

    Install and configure Ruff. Apply linting and formatting fixes.

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,11 @@ Including another Urlconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+
 from feeds import views
 
 urlpatterns = [
