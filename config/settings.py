a6357b2f3e9ba2904e62b31a15c4d02f903c0e4d
Michael LeRoux <946718+mpleroux@users.noreply.github.com>
2026-07-20 21:09:23 -0400

    Settings formatting

--- config/settings.py
+++ config/settings.py
@@ -106,11 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'America/New_York'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
