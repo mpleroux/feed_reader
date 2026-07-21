fc3a0039828ed0471b2bc769df219ec7876e4fa7
Michael LeRoux <946718+mpleroux@users.noreply.github.com>
2026-07-21 14:23:44 -0400

    Add per-feed view and shared article list partial

--- feeds/urls.py
+++ feeds/urls.py
@@ -6,4 +6,5 @@ app_name = 'feeds'
 
 urlpatterns = [
     path('', views.all_items, name='all_items'),
+    path('feed/<int:feed_id>/', views.feed_detail, name='feed_detail'),
 ]
--- feeds/views.py
+++ feeds/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from .models import Article
+from .models import Article, Feed
 
 
 def all_items(request):
     articles = Article.objects.select_related('feed')
     return render(request, 'all_items.html', {'articles': articles})
+
+
+def feed_detail(request, feed_id):
+    feed = get_object_or_404(Feed, pk=feed_id)
+    # noinspection PyUnresolvedReferences
+    articles = feed.articles.all()
+    return render(request, 'feed_detail.html', {'feed': feed, 'articles': articles})
